82758f16532f498945c933bdb4d4305cec16097e
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-06-18 20:06:09 +0530

    Random Image Generating

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,55 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.grey[850],
-          appBar: AppBar(
-            backgroundColor: Colors.grey[700],
-            title: Center(
-              child: Text(
-                "Magic Ball",
-                style: TextStyle(
-                  color: Colors.grey[900],
-                  fontWeight: FontWeight.bold,
-                ),
+        backgroundColor: Colors.grey[850],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[700],
+          title: Center(
+            child: Text(
+              "Magic Ball",
+              style: TextStyle(
+                color: Colors.grey[900],
+                fontWeight: FontWeight.bold,
               ),
             ),
-          )),
+          ),
+        ),
+        body: MagicBall(),
+      ),
     ),
   );
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int imageNum = 1;
+  void randomImageNum() {
+    imageNum = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  randomImageNum();
+                });
+              },
+              child: Image.asset('images/ball${imageNum}.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
